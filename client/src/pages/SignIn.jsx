239528e6b44a37c6a785cd77a16d928d480b6fcc
TheShoaib19 <shoaib.zaki19@hotmail.com>
2024-03-09 10:59:01 +0500

    Complete Sign In page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignIn() {
+  const [ formData, setFormData ] = useState({});
+  const [ error, setError ] = useState(null);
+  const [ loading, setLoading ] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //prevents the page to reload when clicking
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        {/* <div>
+          <p>Choose your role:</p>
+          <div className='flex gap-4'>
+            <label className='flex gap-1'>
+              <input type="radio" name="role" value="vendor" id="role" onChange={handleChange}/>
+              Vendor
+            </label>
+            <label className='flex gap-1'>
+              <input type="radio" name="role" value="consumer" id="role" onChange={handleChange}/>
+              Consumer
+            </label>
+          </div>
+        </div> */}
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account? </p>
+        <Link to="/sign-up">
+          <span className='text-blue-700'>Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
   )
 }
