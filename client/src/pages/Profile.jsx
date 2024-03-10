25b303a23731430eb9a8c7a3cd76393bf31d95b0
TheShoaib19 <shoaib.zaki19@hotmail.com>
2024-03-10 14:16:17 +0500

    Add Sign Out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -95,6 +95,21 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async () => {
+      try {
+        dispatch(signOutUserStart());
+        const res = await fetch('/api/auth/signout');
+        const data = await res.json();
+        if(data.success === false){
+          dispatch(signOutUserFailure(data.message));
+          return;
+        }
+        dispatch(signOutUserSuccess(data));
+      } catch (error) {
+        dispatch(signOutUserFailure(error.message));
+      }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -120,7 +135,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer '>Delete Account</span>
-        <span className='text-red-700 cursor-pointer '>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer '>Sign Out</span>
       </div>
       {isDeleteModalOpen && (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
